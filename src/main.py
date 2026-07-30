12a342deea8ed48527e7ae2d4ffc83e6817da4b3
Tom Pierce <TomPierce@WhalenUS.comm>
2026-07-30 13:45:35 -0700

    Deploy statis site with GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,17 +1,8 @@
 import os
 import shutil
+import sys
 
 from block_markdown import markdown_to_html_node
-
-
-def main() -> None:
-    copy_directory("static", "public")
-
-    generate_pages_recursive(
-        "content",
-        "template.html",
-        "public",
-    )
 
 
 def copy_directory(source: str, destination: str) -> None:
@@ -33,13 +24,15 @@ def copy_directory_contents(source: str, destination: str) -> None:
             shutil.copy(source_path, destination_path)
         else:
             os.mkdir(destination_path)
-            copy_directory_contents(source_path, destination_path)
+
+            copy_directory_contents(
+                source_path,
+                destination_path,
+            )
 
 
 def extract_title(markdown: str) -> str:
-    lines = markdown.split("\n")
-
-    for line in lines:
+    for line in markdown.split("\n"):
         if line.startswith("# "):
             return line[2:].strip()
 
@@ -50,6 +43,7 @@ def generate_page(
     from_path: str,
     template_path: str,
     dest_path: str,
+    basepath: str,
 ) -> None:
     print(
         f"Generating page from {from_path} "
@@ -62,13 +56,21 @@ def generate_page(
     with open(template_path) as template_file:
         template = template_file.read()
 
-    html_node = markdown_to_html_node(markdown)
-    html = html_node.to_html()
-
+    html = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
 
     full_page = template.replace("{{ Title }}", title)
     full_page = full_page.replace("{{ Content }}", html)
+
+    full_page = full_page.replace(
+        'href="/',
+        f'href="{basepath}',
+    )
+
+    full_page = full_page.replace(
+        'src="/',
+        f'src="{basepath}',
+    )
 
     destination_directory = os.path.dirname(dest_path)
 
@@ -79,11 +81,11 @@ def generate_page(
         destination_file.write(full_page)
 
 
-
 def generate_pages_recursive(
     dir_path_content: str,
     template_path: str,
     dest_dir_path: str,
+    basepath: str,
 ) -> None:
     for entry in os.listdir(dir_path_content):
         source_path = os.path.join(dir_path_content, entry)
@@ -91,19 +93,39 @@ def generate_pages_recursive(
 
         if os.path.isfile(source_path):
             if source_path.endswith(".md"):
-                destination_path = destination_path.replace(".md", ".html")
+                destination_path = os.path.splitext(destination_path)[0]
+                destination_path += ".html"
 
                 generate_page(
                     source_path,
                     template_path,
                     destination_path,
+                    basepath,
                 )
         else:
             generate_pages_recursive(
                 source_path,
                 template_path,
                 destination_path,
+                basepath,
             )
+
+
+def main() -> None:
+    basepath = "/"
+
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    copy_directory("static", "docs")
+
+    generate_pages_recursive(
+        "content",
+        "template.html",
+        "docs",
+        basepath,
+    )
+
 
 if __name__ == "__main__":
     main()
